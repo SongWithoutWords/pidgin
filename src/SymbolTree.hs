e7dc56303e867f220bf2458edf0ddf71f4f77a25
ian <ianmccall93@gmail.com>
2017-03-18 00:32:17 -0700

    Ongoing work on SymbolTree module

--- src/SymbolTree.hs
+++ src/SymbolTree.hs
@@ -1,3 +1,26 @@
-module SymbolTree where
+module SymbolTree(astToSymbolTree) where
 
 import qualified Syntax as Syn
+
+
+astToSymbolTree :: Syn.Ast -> SymbolTree
+astToSymbolTree _ = []
+
+type SymbolTree = [Unit]
+
+data Unit
+  = UnitNamespace String [Unit]
+  | UnitClass Class
+  | UnitFunction String Syn.Type
+
+data Class
+  = Class String [Member]
+
+data Member
+  = MemberClass Syn.AccessMod Class
+  | MemberFunction Syn.AccessMod Function
+  | MemberVariable Syn.AccessMod Variable
+
+data Function
+  = 
+
