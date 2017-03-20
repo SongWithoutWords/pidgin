7a3b3116ae74a3041aac70cc1fed0a328192b013
ian <ianmccall93@gmail.com>
2017-03-19 23:26:37 -0700

    Added generation and logging of SymTable to Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,28 @@ module Main where
 
 import Lexer
 import Parser
+import SymbolTree
 
 import FormatAst
 
 main :: IO ()
 main = do
   input <- readFile "in/input.pgn"
+
   let tokens = scanTokens input
   let tokenFilePath = "out/tokens.txt"
   putStrLn $ "Writing tokens to " ++ tokenFilePath
   writeFile tokenFilePath (formatTokens tokens)
 
-  let rawAst = show $ parse tokens
-  let prettyAst = formatAst rawAst
+  let ast = parse tokens
+
+  let prettyAst = formatAst $ show ast
   let astFilePath = "out/ast.txt"
   putStrLn $ "Writing AST to " ++ astFilePath
   writeFile astFilePath prettyAst
+
+  let symTable = symTableFromAst ast
+  let symTablePath = "out/symtable.txt"
+  putStrLn $ "Writing SymTable to " ++ symTablePath
+  writeFile symTablePath $ show symTable
+
