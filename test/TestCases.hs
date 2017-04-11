815d40ed7b1c73029060e6175da8b0c12b95baf1
ian <ianmccall93@gmail.com>
2017-04-11 00:37:57 -0700

    Added tokens+ast for test cases, and removed a duplicate test case

--- test/TestCases.hs
+++ test/TestCases.hs
@@ -42,17 +42,10 @@ testCases =
     }
 
   , TestCase
-    { nme = "op expr"
-    , src = "$ three = 1 + 2"
-    , tks = [T.Dollar, T.Name "three", T.Equal, T.LitInt 1, T.Plus, T.LitInt 2]
-    , ast = [UVar $ Var (TypedName (TInferred Immutable) "three") $ EApply $ Apply (ESelect $ Select (ELitInt 1) "+") [ELitInt 2]]
-    }
-
-  , TestCase
     { nme = "def negate inline"
     , src = "negate(Bln b) -> Bln => false if b else true"
     , tks = [T.Name "negate", T.LParen, T.TypeBln, T.Name "b", T.RParen, T.ThinArrow, T.TypeBln, T.FatArrow, T.False, T.If, T.Name "b", T.Else, T.True]
-    , ast = [UFunc $ Func (Sig "negate" $ AnonSig Pure [TypedName (TBln Immutable) "b"] (TBln Mutable)) [SExpr $ EIf (ELitBln False) (EName "b") (ELitBln True)]]
+    , ast = [UFunc $ Func (Sig "negate" $ AnonSig Pure [TypedName (TBln Immutable) "b"] (TBln Immutable)) [SExpr $ EIf (ELitBln False) (EName "b") (ELitBln True)]]
     }
 
   , TestCase
@@ -61,35 +54,48 @@ testCases =
     , src = "negate(Bln b) -> Bln =>\n\
             \    false if b else true"
 
-    , tks = [ T.Name "negate", T.LParen, T.TypeBln, T.Name "b", T.RParen, T.ThinArrow, T.TypeBln, T.FatArrow,
-              T.Indent,
-              T.False, T.If, T.Name "b", T.Else, T.True,
-              T.Dedent]
+    , tks = [ T.Name "negate", T.LParen, T.TypeBln, T.Name "b", T.RParen, T.ThinArrow, T.TypeBln, T.FatArrow
+            , T.Indent
+            , T.False, T.If, T.Name "b", T.Else, T.True
+            , T.Dedent]
 
-    , ast = []
+    , ast = [UFunc $ Func (Sig "negate" $ AnonSig Pure [TypedName (TBln Immutable) "b"] $ TBln Immutable) [SExpr $ EIf (ELitBln False) (EName "b") (ELitBln True)]]
     }
 
   , TestCase
     { nme = "def factorial"
 
-    , src = "factorial(Int n) -> Int =>\n\
+    , src = "factorial(Nat n) -> Nat =>\n\
             \    1 if n <= 0 else n * factorial(n-1)"
 
-    , tks = [ T.Name "factorial", T.LParen, T.TypeInt, T.Name "n", T.RParen, T.ThinArrow, T.TypeInt, T.FatArrow
+    , tks = [ T.Name "factorial", T.LParen, T.TypeNat, T.Name "n", T.RParen, T.ThinArrow, T.TypeNat, T.FatArrow
             , T.Indent
-            , T.LitInt 1, T.If, T.Name "n", T.LesserOrEq, T.LitInt 0, T.Else, T.Name "n", T.Star, T.Name "factorial", T.LParen, T.Name "n", T.Minus, T.LitInt 1, T.RParen
+            , T.LitInt 1, T.If, T.Name "n", T.LesserEq, T.LitInt 0, T.Else, T.Name "n", T.Star, T.Name "factorial", T.LParen, T.Name "n", T.Minus, T.LitInt 1, T.RParen
             , T.Dedent]
 
-    , ast = []
+    , ast = [ UFunc $ Func (Sig "factorial" $ AnonSig Pure [TypedName (TNat Immutable) "n"] $ TNat Immutable)
+              [ SExpr $ EIf
+                (ELitInt 1)
+                (EApply $ Apply (ESelect $ Select (EName "n") "<=") [ELitInt 0])
+                $ EApply $ Apply (ESelect $ Select (EName "n") "*")
+                [EApply $ Apply (EName "factorial") [EApply $ Apply (ESelect $ Select (EName "n") "-") [ELitInt 1]]] ]]
     }
 
   , TestCase
     { nme = "draw widget"
-    , src = "drawWidget(Nat width, Nat height) -> None\n\
+    , src = "drawWidget(Nat width, Nat height) -> None =>\n\
             \    $ w = Widget(width, height)\n\
             \    if w.exists\n\
             \        w.@draw()\n"
-    , tks = []
+    , tks = [ T.Name "drawWidget", T.LParen, T.TypeNat, T.Name "width", T.Comma, T.TypeNat, T.Name "height", T.RParen, T.ThinArrow, T.TypeNone
+            , T.Indent
+              , T.Dollar, T.Name "w", T.Equal, T.Typename "Widget", T.LParen, T.Name "width", T.Comma, T.Name "height", T.RParen
+              , T.Eol
+              , T.If, T.Name "w", T.Dot, T.Name "exists"
+              , T.Indent
+                , T.Name "w", T.Dot, T.At, T.Name "draw", T.LParen, T.RParen
+              , T.Dedent
+            , T.Dedent]
     , ast = []
     }
   ]
