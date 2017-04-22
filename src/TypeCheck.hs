d3ab3ca35ee1a201582df0253222675252598f13
ian <ianmccall93@gmail.com>
2017-04-21 23:19:46 -0700

    Ongoing work ont TypeCheck module

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -1,10 +1,12 @@
 {-# language FlexibleInstances #-}
 {-# language TypeSynonymInstances #-}
 
-module TypeCheck(typeCheck) where
+module TypeCheck(typeCheck, Result(..)) where
 
 import Ast
 import TypeErrors
+
+-- TODO: I think this pattern may lend itself to a monad
 
 data Result a = Result { typedResult :: a, errors :: TypeErrors } deriving(Eq, Show)
 
@@ -17,16 +19,34 @@ instance Checked Ast where
     in Result (map typedResult uResults) (concatMap errors uResults)
 
 instance Checked Unit where
-  typeCheck u = case u of
-    UNamespace name us ->
-      let usChecked = map typeCheck us
-      in Result (UNamespace name $ map typedResult usChecked) (concatMap errors usChecked)
-    c@UClass {} -> Result c []
-    f@UFunc {} -> Result f []
+  typeCheck unit = case unit of
+    UNamespace name units ->
+      let unitsChkd = map typeCheck units
+      in Result (UNamespace name $ map typedResult unitsChkd) (concatMap errors unitsChkd)
+    c@UClass {} -> Result c [] -- TODO
+    f@UFunc {} -> Result f [] -- TODO
     UVar v ->
       let vRes = typeCheck v
       in Result (UVar $ typedResult vRes) (errors vRes)
 
 instance Checked Var where
-  typeCheck Var {} = undefined
+  typeCheck v@(Var (TypedName typeLhs name) rhs) =
+    let typeRhs = typeOf rhs
+    in case typeLhs of
+      TInferred _ -> Result (Var (TypedName typeRhs name) rhs) []
+      _ -> Result v $ if typeLhs `assignableFrom` typeRhs then [] else [TypeConflict typeLhs typeRhs]
+
+assignableFrom :: Type -> Type -> Bool
+assignableFrom a b = a == b
+
+typeOf :: Expr -> Type
+typeOf e = case e of
+  ELitBln _ -> TBln Mutable
+  ELitChr _ -> TChr Mutable
+  ELitFlt _ -> TFlt Mutable
+  ELitInt _ -> TInt Mutable
+  ELitStr _ -> TStr Mutable
+
+
+
 
