ec07d0e5a0e6ffdfece591fe5127458f59f1dfce
ian <ianmccall93@gmail.com>
2017-05-06 22:03:22 -0700

    Added FailedToUnify type error, and removed dependency on Ast

--- src/TypeErrors.hs
+++ src/TypeErrors.hs
@@ -1,10 +1,13 @@
 module TypeErrors where
 
-import Ast
+import Types
+
+import qualified Data.Set as Set
 
 data Error
   = UnknownId String
   | TypeConflict { expected :: Type, received :: Type }
+  | FailedToUnify (Set.Set Type)
   | NonApplicable Type
   | ArgCount { acExpected :: Int, acReceived :: Int }
   deriving(Eq, Show)
