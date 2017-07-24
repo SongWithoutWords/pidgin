6194736c5286f764a49752bbaeb95883af9323b0
ian <ianmccall93@gmail.com>
2017-07-23 23:29:12 -0700

    [MultiMap] renamed MultiMapInstances->MultiMap and re-exported Data.MultiMap

--- src/MultiMap.hs
+++ src/MultiMap.hs
@@ -1,34 +1,35 @@
 {-# language TypeSynonymInstances #-}
 {-# language InstanceSigs #-}
 
-module MultiMapInstances where
+module MultiMapInstances
+  ( module MultiMapInstances
+  , module Data.MultiMap
+  ) where
 
-import qualified Data.MultiMap as Multi
-
-type MultiMap = Multi.MultiMap
+import Data.MultiMap
 
 instance (Eq k, Eq a) => Eq (MultiMap k a) where
-  (==) x y = Multi.toMap x == Multi.toMap y
+  (==) x y = toMap x == toMap y
 
 instance (Show k, Show a) => Show (MultiMap k a) where
-  show = show . Multi.toMap
+  show = show . toMap
 
 union :: MultiMap k a -> MultiMap k a -> MultiMap k a
 union = undefined
 
 instance Monoid (MultiMap k a) where
-  mempty = Multi.empty
+  mempty = empty
   mappend = union
 
 instance Functor (MultiMap k) where
   fmap :: (a -> b) -> MultiMap k a -> MultiMap k b
   fmap = undefined
 
-instance Foldable (Multi.MultiMap k) where
+instance Foldable (MultiMap k) where
   foldr :: (a -> b -> b) -> b -> (MultiMap k a) -> b
   foldr = undefined
 
-instance Traversable (Multi.MultiMap k) where
+instance Traversable (MultiMap k) where
   traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
   traverse = undefined
 
