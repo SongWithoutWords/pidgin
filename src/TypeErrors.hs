99d351d47a85abd2527b12bd014a3e80f28ee52e
ian <ianmccall93@gmail.com>
2017-07-23 20:24:17 -0700

    [TypeError] added NeedExprFoundType and NeedExprFoundNamespace

--- src/TypeErrors.hs
+++ src/TypeErrors.hs
@@ -5,13 +5,26 @@ import Types
 import qualified Data.Set as Set
 
 data Error
+
   = UnknownId String
+
+  -- Better conflict or mismatch?
   | TypeConflict { expected :: Type, received :: Type }
+
+  -- Could be called incompatible types? No common supertype?
   | FailedToUnify (Set.Set Type)
+
   | NonApplicable Type
   | ArgCount { acExpected :: Int, acReceived :: Int }
+
+  -- Should be multiple, competing, duplicate, overlapping, contrandictory?...
   | CompetingDefinitions
+
   | RecursiveDefinition
+
+  | NeedExprFoundType
+  | NeedExprFoundNamespace
+
   deriving(Eq, Show)
 
 type Errors = [Error]
