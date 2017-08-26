8c3732142e137ba74e0ce69d756cf9f8ebaccf88
ian <ianmccall93@gmail.com>
2017-08-26 00:16:39 -0700

    [Preface] added takeWhileInclusive

--- src/Preface.hs
+++ src/Preface.hs
@@ -26,3 +26,9 @@ consMaybe Nothing xs = xs
 (?:) :: Maybe a -> [a] -> [a]
 (?:) = consMaybe
 
+takeWhileInclusive :: (a -> Bool) -> [a] -> [a]
+takeWhileInclusive _ [] = []
+takeWhileInclusive p (x:xs) = x : if p x
+  then takeWhileInclusive p xs
+  else []
+
