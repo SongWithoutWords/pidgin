997cb30f193cc9f0fd67df80b032f8bffdf906ae
ian <ianmccall93@gmail.com>
2017-08-25 20:18:58 -0700

    [MultiMap] added multiMapWithKeyM (will be used for typechecking)

--- src/MultiMap.hs
+++ src/MultiMap.hs
@@ -19,5 +19,8 @@ multiInsert k v = insertWith (++) k [v]
 multiFromList :: Ord k => [(k, a)] -> Map k [a]
 multiFromList pairs = Prelude.foldl (\m (k, v) -> multiInsert k v m) (Data.Map.empty) pairs
 
+multiMapWithKeyM :: (Monad m, Ord k)  => (k -> a -> m b) -> Map k [a] -> m (Map k [b])
+multiMapWithKeyM f multiMap = sequence $ mapWithKey (\k xs -> mapM (f k) xs) multiMap
+
 -- TODO: see Checked [A1.Ast] and checked [A1.Unit] for multiMapM motivation
 
