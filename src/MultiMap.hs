3d1b5157b9f7383146de4a38fd4ddfb6b41cbf6d
ian <ianmccall93@gmail.com>
2017-09-10 14:38:55 -0700

    [MultiMap] added multiMapWithKey, qualified import of Data.Map

--- src/MultiMap.hs
+++ src/MultiMap.hs
@@ -1,24 +1,27 @@
 
 module MultiMap
   ( module MultiMap
-  , Map
+  , M.Map
   ) where
 
-import Data.Map
+import qualified Data.Map as M
 
-type MultiMap k a = Map k [a]
+type MultiMap k a = M.Map k [a]
 
-multiLookup :: Ord k => k -> Map k [a] -> [a]
-multiLookup k m = case Data.Map.lookup k m of
+multiLookup :: Ord k => k -> M.Map k [a] -> [a]
+multiLookup k m = case M.lookup k m of
   Nothing -> []
   Just values -> values
 
-multiInsert :: Ord k => k -> a -> Map k [a] -> Map k [a]
-multiInsert k v = insertWith (++) k [v]
+multiInsert :: Ord k => k -> a -> M.Map k [a] -> M.Map k [a]
+multiInsert k v = M.insertWith (++) k [v]
 
-multiFromList :: Ord k => [(k, a)] -> Map k [a]
-multiFromList pairs = Prelude.foldl (\m (k, v) -> multiInsert k v m) (Data.Map.empty) pairs
+multiFromList :: Ord k => [(k, a)] -> M.Map k [a]
+multiFromList pairs = Prelude.foldl (\m (k, v) -> multiInsert k v m) (M.empty) pairs
 
-multiMapWithKeyM :: (Monad m, Ord k)  => (k -> a -> m b) -> Map k [a] -> m (Map k [b])
-multiMapWithKeyM f multiMap = sequence $ mapWithKey (\k xs -> mapM (f k) xs) multiMap
+multiMapWithKey :: (k -> a -> b) -> M.Map k [a] -> M.Map k [b]
+multiMapWithKey f multi = M.mapWithKey (\k xs -> Prelude.map (f k) xs) multi
+
+multiMapWithKeyM :: (Monad m, Ord k)  => (k -> a -> m b) -> M.Map k [a] -> m (M.Map k [b])
+multiMapWithKeyM f multiMap = sequence $ M.mapWithKey (\k xs -> mapM (f k) xs) multiMap
 
