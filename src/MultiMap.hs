85f902d2e8e3703b6b5c9eb01b1d9ed1cfda1315
ian <ianmccall93@gmail.com>
2017-09-10 14:51:17 -0700

    [MultiMap] added multiMapFoldWithKey

--- src/MultiMap.hs
+++ src/MultiMap.hs
@@ -20,8 +20,11 @@ multiFromList :: Ord k => [(k, a)] -> M.Map k [a]
 multiFromList pairs = Prelude.foldl (\m (k, v) -> multiInsert k v m) (M.empty) pairs
 
 multiMapWithKey :: (k -> a -> b) -> M.Map k [a] -> M.Map k [b]
-multiMapWithKey f multi = M.mapWithKey (\k xs -> Prelude.map (f k) xs) multi
+multiMapWithKey f = M.mapWithKey (\k xs -> Prelude.map (f k) xs)
+
+multiMapFoldWithKey :: (k -> a -> b) -> M.Map k [a] -> [b]
+multiMapFoldWithKey f = M.foldMapWithKey (\k xs -> Prelude.map (f k) xs)
 
 multiMapWithKeyM :: (Monad m, Ord k)  => (k -> a -> m b) -> M.Map k [a] -> m (M.Map k [b])
-multiMapWithKeyM f multiMap = sequence $ M.mapWithKey (\k xs -> mapM (f k) xs) multiMap
+multiMapWithKeyM f m = sequence $ M.mapWithKey (\k xs -> mapM (f k) xs) m
 
