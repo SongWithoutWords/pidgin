2b4156dc418baef4ad144188b209e40be7d5bc98
ian <ianmccall93@gmail.com>
2017-09-10 14:24:56 -0700

    [Llvm] working execMainOfLlvmAst :)

--- src/LlvmUtil.hs
+++ src/LlvmUtil.hs
@@ -1,19 +1,50 @@
+{-# language OverloadedStrings #-}
 module LlvmUtil where
 
 import qualified Data.ByteString.Char8 as C8
+import Foreign.Ptr(FunPtr, castFunPtr)
 
-import qualified LLVM.AST as LAst
-import qualified LLVM.Context as LCont
-import qualified LLVM.ExecutionEngine as LExec
-import qualified LLVM.Module as LMod
+import qualified LLVM.AST as A
+import qualified LLVM.Context as C
+import qualified LLVM.ExecutionEngine as EE
+import qualified LLVM.Module as M
 
--- Give me an Llvm Ast + an action to run on the resulting module, and I'll do that for you :)
-withModuleFromAst :: LAst.Module -> (LCont.Context -> LMod.Module -> IO a) -> IO a
-withModuleFromAst astMod action = LCont.withContext $ \context ->
-  LMod.withModuleFromAST context astMod $ \lmod -> action context lmod
 
-llvmAstToAsm :: LAst.Module -> IO String
+foreign import ccall "dynamic" exec :: FunPtr (IO Int) -> (IO Int)
+
+-- Llvm Ast + action to run on resulting module -> good times
+withModuleFromAst :: A.Module -> (C.Context -> M.Module -> IO a) -> IO a
+withModuleFromAst astMod action = C.withContext $ \context ->
+  M.withModuleFromAST context astMod $ \lmod -> action context lmod
+
+llvmAstToAsm :: A.Module -> IO String
 llvmAstToAsm astMod = withModuleFromAst astMod $ \_ lmod -> do
-  llstr <- LMod.moduleLLVMAssembly lmod
+  llstr <- M.moduleLLVMAssembly lmod
   return $ C8.unpack llstr
+
+withJit :: C.Context -> (EE.MCJIT -> IO a) -> IO a
+withJit context action = EE.withMCJIT
+  context
+  (Just 0) -- optimization level
+  Nothing  -- model
+  Nothing  -- frame pointer elimination
+  Nothing  -- fast instruction selecti
+  action
+
+execFunction :: FunPtr a -> IO Int
+execFunction fn = exec (castFunPtr fn :: FunPtr (IO Int))
+
+execMainOfLlvmAst :: A.Module -> IO (Maybe Int)
+execMainOfLlvmAst astMod = withModuleFromAst astMod $ \context lmod ->
+  withJit context $ \mcjit ->
+    EE.withModuleInEngine mcjit lmod $ \ee -> do
+      main <- EE.getFunction ee (A.Name "main")
+      case main of
+        Just f -> do
+          res <- execFunction f
+          putStrLn $ "Evaluated to: " ++ show res
+          return $ Just res
+        Nothing -> do
+          putStrLn "No main function"
+          return Nothing
 
