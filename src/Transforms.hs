83568712d558c41ad60a1437ea228b8c6f9f3b4c
ian <ianmccall93@gmail.com>
2017-09-11 22:30:56 -0700

    [Transforms] added lexParseCheckGen and pidginToLlvmIr

--- src/Transforms.hs
+++ src/Transforms.hs
@@ -1,19 +1,31 @@
 module Transforms
   ( lexParseCheck
+  , lexParseCheckGen
+  , pidginToLlvmIr
   ) where
 
 import System.IO.Unsafe
 
+import LLVM.AST as A
+
 import Ast
 import MultiMapAst
-import TypeErrors
 
 import Lexer
 import Parser
 import TypeCheck
+import CodeGen
+import LlvmUtil
 
 (|>) :: (a -> b) -> (b -> c) -> (a -> c)
 f |> g = g . f
 
 lexParseCheck :: String -> (AstMc, Errors)
 lexParseCheck = scanTokens |> parse |> multiMapAst |> typeCheckAst
+
+lexParseCheckGen :: String -> A.Module
+lexParseCheckGen = (fst . lexParseCheck) |> codeGen
+
+pidginToLlvmIr :: String -> IO String
+pidginToLlvmIr src = llvmAstToAsm $ lexParseCheckGen src
+
