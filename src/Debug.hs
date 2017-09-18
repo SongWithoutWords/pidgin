adc4d5d50b3c8c2aad113ecfe09f8944c59d0751
ian <ianmccall93@gmail.com>
2017-09-17 18:04:02 -0700

    [Debug] added traceShowId and traceAppend

--- src/Debug.hs
+++ src/Debug.hs
@@ -9,6 +9,12 @@ debug = False
 trace :: String -> a -> a
 trace s a = if debug then Trace.trace s a else a
 
+traceShowId :: Show a => a -> a
+traceShowId = if debug then Trace.traceShowId else id
+
+traceAppend :: Show a => String -> a -> a
+traceAppend s a = trace (s ++ show a) a
+
 traceM :: Monad m => String -> m ()
 traceM s = if debug then Trace.traceM s else return ()
 
