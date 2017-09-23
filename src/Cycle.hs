539b5494c09fe6462ba5850e077c2f4cf1a00297
ian <ianmccall93@gmail.com>
2017-09-23 14:57:47 -0700

    [TypeCheck] created generic cycle data type for equality between cycles

---
+++ src/Cycle.hs
@@ -0,0 +1,18 @@
+module Cycle where
+
+shiftLeft :: [a] -> [a]
+shiftLeft [] = []
+shiftLeft [x] = [x]
+shiftLeft (x:xs) = xs ++ [x]
+
+data Cycle a = Cycle [a]
+  deriving(Ord, Show)
+
+instance Eq a => Eq (Cycle a) where
+  (Cycle xs) == (Cycle ys) = cycleEq xs
+    where
+      cycleEq xsShifted =
+        if xsShifted == ys || xsShifted == reverse ys then True
+        else if (shiftLeft xsShifted) == xs then False
+        else cycleEq (shiftLeft xsShifted)
+
