198d697637c3a9503e98e1db449238f42610cade
ian <ianmccall93@gmail.com>
2017-10-05 14:52:47 -0700

    [Unify] wrapped up work for the time being on the Unify module

--- src/TypeCheck/Unify.hs
+++ src/TypeCheck/Unify.hs
@@ -1,8 +1,9 @@
+{-# language GADTs #-}
 
 module TypeCheck.Unify
   ( unifyConstraints
-  , module Constraint
-  , module Substitution
+  , module TypeCheck.Constraint
+  , module TypeCheck.Substitution
   ) where
 
 import Data.Map as M
@@ -10,9 +11,15 @@ import Data.Map as M
 import TypeCheck.Constraint
 import TypeCheck.Substitution
 
+-- TODO: put this in a Writer Error monad, and swap out raise for tell
+raise :: Error -> a
+raise e = error $ "Unification error " ++ show e
+
 unifyConstraints :: [Constraint] -> Substitutions
 unifyConstraints [] = M.empty
 unifyConstraints (c : cs) =
+
+  -- TODO: attempt nicer re-write when test-coverage is in place
   let s2 = unifyConstraints cs in
   let s1 = unifyConstraint $ subConstraint s2 c in
   let s2' = M.map (subType s1) s2 in
@@ -20,22 +27,28 @@ unifyConstraints (c : cs) =
 
 unifyConstraint :: Constraint -> Substitutions
 unifyConstraint (t1 := t2) = let
-  unify' :: Type -> Type -> Substitutions
+  unify' :: Type2 -> Type2 -> Substitutions
   unify' a b
 
     | a == b = M.empty
 
-    | TVar x <- a = M.singleton x b
+    | TVar aVar <- a = M.singleton aVar b
 
-    | TVar y <- b = M.singleton y a
+    | TVar bVar <- b = M.singleton bVar a
 
-    | TLam x1 x2 <- a
-    , TLam y1 y2 <- b
-    = unifyConstraints [x1 := y1, x2 := y2]
+    -- TODO: Handle purity
+    | TFunc _ aParams aRet <- a
+    , TFunc _ bParams bRet <- b
+    = unifyConstraints $ constrainParamTypes aParams bParams ++ [aRet := bRet]
 
     | otherwise = error $ "cannot unify " ++ show a ++ " " ++ show b
 
   in unify' t1 t2
+
+constrainParamTypes :: [Type2] -> [Type2] -> [Constraint]
+constrainParamTypes xs ys = if length xs == length ys
+  then zipWith (\x y -> x := y) xs ys
+  else raise $ WrongNumArgs (length xs) (length ys)
 
 subConstraint :: Substitutions -> Constraint -> Constraint
 subConstraint = mapConstraint . subType
