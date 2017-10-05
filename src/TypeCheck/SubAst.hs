2f6be2191dc16658d228ad06821ad6bfc566ded6
ian <ianmccall93@gmail.com>
2017-10-05 11:52:05 -0700

    [Unify] ongoing work on SubAst

--- src/TypeCheck/SubAst.hs
+++ src/TypeCheck/SubAst.hs
@@ -4,22 +4,41 @@ module TypeCheck.SubAst
   ( subAst
   ) where
 
+import Control.Monad.Reader
+
 import Ast
+import MultiMap
 import TypeCheck.Substitution
 
-subAst :: Substitutions -> AstT -> AstT
-subAst s (Ast namedExprs expr) =
-  Ast (map (fmap $ subExpr s) namedExprs) (subExpr s expr)
+-- type SubM a = Reader Substitutions a
 
-subExpr :: Substitutions -> ExprT -> ExprT
-subExpr s (ExprT t e) = ExprT (subType s t) (subExpr' s e)
+-- sub :: Type2 -> SubM Type2
+-- sub = ask >>= subType
+  -- env <- ask
+  -- subType env
 
-subExpr' :: Substitutions -> ExprT' -> ExprT'
+
+subAst :: Substitutions -> Ast2 -> Ast2
+subAst s = multiMap (subUnit s)
+
+subUnit :: Substitutions -> Unit2 -> Unit2
+subUnit s unit = case unit of
+  UNamespace1 units -> UNamespace1 $ subAst s units
+  UFunc f -> UFunc $ subFunc s f
+  UVar v -> UVar $ subVar v
+
+subFunc :: Substitutions -> Func2 -> Func2
+subFunc s (Func sig b) = Func (subSig s sig) (subBlock s b)
+
+subExpr :: Substitutions -> Expr2 -> Expr2
+subExpr s (Expr2 t e) = Expr2 (subType s t) (subExpr' s e)
+
+subExpr' :: Substitutions -> Expr2' -> Expr2'
 subExpr' s expr = let sub = subExpr s in case expr of
-  EVar n -> EVar n
-  ELamT (Named n t) e -> ELamT (Named n $ subType s t) $ sub e
+  EName n -> EName n
+  ELambda (Named n t) e -> ELamT (Named n $ subType s t) $ sub e
   EApp e1 e2 -> EApp (sub e1) (sub e2)
-  EIf (Pred e1) e2 e3 -> EIf (Pred $ sub e1) (sub e2) (sub e3)
+  EIf e1 e2 e3 -> EIf sub e1 (sub e2) (sub e3)
   EBinOp op e1 e2 -> EBinOp op (sub e1) (sub e2)
   EVal v -> EVal v
 
