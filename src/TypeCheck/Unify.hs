3556415082b3e20b6e1c14b6e37d2687211cfe35
ian <ianmccall93@gmail.com>
2017-10-10 22:03:14 -0700

    [Unify] moved unification into new ErrorM for type error reporting

--- src/TypeCheck/Unify.hs
+++ src/TypeCheck/Unify.hs
@@ -3,52 +3,53 @@
 module TypeCheck.Unify
   ( unifyConstraints
   , module TypeCheck.Constraint
+  , module TypeCheck.ErrorM
   , module TypeCheck.Substitution
   ) where
 
 import Data.Map as M
+-- import Control.Monad(zipWithM)
 
 import TypeCheck.Constraint
+import TypeCheck.ErrorM
 import TypeCheck.Substitution
 
--- TODO: put this in a Writer Error monad, and swap out raise for tell
-raise :: Error -> a
-raise e = error $ "Unification error " ++ show e
+unifyConstraints :: [Constraint] -> ErrorM Substitutions
+unifyConstraints [] = pure M.empty
+unifyConstraints (c : cs) = do
+  s2 <- unifyConstraints cs
+  s1 <- unifyConstraint $ subConstraint s2 c
+  let s2' = M.map (subType s1) s2
+  pure $ M.union s1 s2'
 
-unifyConstraints :: [Constraint] -> Substitutions
-unifyConstraints [] = M.empty
-unifyConstraints (c : cs) =
-
-  -- TODO: attempt nicer re-write when test-coverage is in place
-  let s2 = unifyConstraints cs in
-  let s1 = unifyConstraint $ subConstraint s2 c in
-  let s2' = M.map (subType s1) s2 in
-    M.union s1 s2'
-
-unifyConstraint :: Constraint -> Substitutions
+unifyConstraint :: Constraint -> ErrorM Substitutions
 unifyConstraint (t1 := t2) = let
-  unify' :: Type2 -> Type2 -> Substitutions
+  unify' :: Type2 -> Type2 -> ErrorM Substitutions
   unify' a b
 
-    | a == b = M.empty
+    | a == b = pure M.empty
 
-    | TVar aVar <- a = M.singleton aVar b
+    | TVar a' <- a = pure $ M.singleton a' b
+    | TVar b' <- b = pure $ M.singleton b' a
 
-    | TVar bVar <- b = M.singleton bVar a
+    | TError a' <- a = raise a' >> pure M.empty
+    | TError b' <- b = raise b' >> pure M.empty
 
     -- TODO: Handle purity
     | TFunc _ aParams aRet <- a
     , TFunc _ bParams bRet <- b
-    = unifyConstraints $ constrainParamTypes aParams bParams ++ [aRet := bRet]
+      = do
+          paramConstraints <- constrainParamTypes aParams bParams
+          unifyConstraints $ paramConstraints ++ [aRet := bRet]
 
-    | otherwise = error $ "cannot unify " ++ show a ++ " " ++ show b
+    | otherwise = raise (FailedToUnify a b) >> pure M.empty
 
   in unify' t1 t2
 
-constrainParamTypes :: [Type2] -> [Type2] -> [Constraint]
+constrainParamTypes :: [Type2] -> [Type2] -> ErrorM [Constraint]
 constrainParamTypes xs ys = if length xs == length ys
-  then zipWith (\x y -> x := y) xs ys
-  else raise $ WrongNumArgs (length xs) (length ys)
+  then pure $ zipWith (\x y -> x := y) xs ys
+  else raise (WrongNumArgs (length xs) (length ys)) >> pure []
 
 subConstraint :: Substitutions -> Constraint -> Constraint
 subConstraint = mapConstraint . subType
