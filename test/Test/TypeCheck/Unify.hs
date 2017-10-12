6b4cea383a4de691a75d499fad864be3b6ef5c9e
ian <ianmccall93@gmail.com>
2017-10-11 22:23:37 -0700

    [Unify] updated test cases

--- test/Test/TypeCheck/Unify.hs
+++ test/Test/TypeCheck/Unify.hs
@@ -7,73 +7,69 @@ import Test.Tasty.HUnit
 
 import TypeCheck.Unify
 
+type SubstitutionList = [(Word, Type2)]
+
+unifyTest :: String -> [Constraint] -> SubstitutionList -> [Error] -> TestTree
+unifyTest name constraints subs errors =
+  let result = runWriter $ unifyConstraints constraints
+  in testGroup name
+    [ testCase "substitutions" $ fst result @?= M.fromList subs
+    , testCase "errors" $ snd result @?= errors
+    ]
+
 unifyTests :: TestTree
 unifyTests = testGroup "Unification"
-  [ test00
-  , test01
-  , test02
-  , test03
-  , test04
-  , test05
-  , test06
-  , test07
-  , test08
+  [ unifyTest "00 - empty" [] [] []
+
+  , unifyTest "01 - single"
+    [TVar 0 := TInt]
+    [(0, TInt)]
+    []
+
+  , unifyTest "02 - simple sub"
+    [TVar 0 := TVar 1, TVar 1 := TInt]
+    [(0, TInt), (1, TInt)]
+    []
+
+  , unifyTest "03 - multiple subs"
+    [TVar 0 := TVar 1, TVar 1 := TVar 2, TVar 2 := TInt]
+    [(0, TInt), (1, TInt), (2, TInt)]
+    []
+
+  , unifyTest "04 - lambda"
+    [TVar 0 := TFunc Pure [TVar 1] (TVar 2), TVar 1 := TVar 2, TVar 2 := TInt]
+    [(0, TFunc Pure [TInt] TInt), (1, TInt), (2, TInt)]
+    []
+
+  , unifyTest "05 - lambda equality one sided"
+    [TFunc Pure [TVar 0] (TVar 1) := TFunc Pure [TBln] TInt]
+    [(0, TBln), (1, TInt)]
+    []
+
+  , unifyTest "06 - lambda two sided"
+    [TFunc Pure [TVar 0] TInt := TFunc Pure [TBln] (TVar 1)]
+    [(0, TBln), (1, TInt)]
+    []
+
+  , unifyTest "07 - typeVar := lambda"
+    [ TVar 0 := TFunc Pure [TVar 1] TInt
+    , TVar 0 := TFunc Pure [TBln] (TVar 2)
+    ]
+    [(0, TFunc Pure [TBln] TInt), (1, TBln), (2, TInt)]
+    []
+
+  , unifyTest "08 - inc constraints"
+    [ TInt := TVar 1
+    , TInt := TInt
+    , TInt := TVar 2
+    , TVar 0 := TFunc Pure [TVar 1] (TVar 2)
+    , TVar 0 := TFunc Pure [TInt] (TVar 3)
+    ]
+    [ (0, TFunc Pure [TInt] TInt)
+    , (1, TInt)
+    , (2, TInt)
+    , (3, TInt)
+    ]
+    []
   ]
 
-unifyTest :: String -> [Constraint] -> Substitutions -> TestTree
-unifyTest name c s = testCase name $ unifyConstraints c @?= s
-
-test00 = unifyTest "00 - empty" c s
-  where
-    c = []; s = M.empty
-
-test01 = unifyTest "01 - single" c s
-  where
-    c = [TVar 0 := TInt]
-    s = M.singleton 0 TInt
-
-test02 = unifyTest "02 - simple sub" c s
-  where
-    c = [TVar 0 := TVar 1, TVar 1 := TInt]
-    s = M.fromList [(0, TInt), (1, TInt)]
-
-test03 = unifyTest "03 - multiple subs" c s
-  where
-    c = [TVar 0 := TVar 1, TVar 1 := TVar 2, TVar 2 := TInt]
-    s = M.fromList [(0, TInt), (1, TInt), (2, TInt)]
-
-test04 = unifyTest "04 - lambda" c s
-  where
-    c = [TVar 0 := TFunc Pure [TVar 1] (TVar 2), TVar 1 := TVar 2, TVar 2 := TInt]
-    s = M.fromList [(0, TFunc Pure [TInt] TInt), (1, TInt), (2, TInt)]
-
-test05 = unifyTest "05 - lambda equality one sided" c s
-  where
-    c = [TFunc Pure [TVar 0] (TVar 1) := TFunc Pure [TBln] TInt]
-    s = M.fromList [(0, TBln), (1, TInt)]
-
-test06 = unifyTest "06 - lambda two sided" c s
-  where
-    c = [TFunc Pure [TVar 0] TInt := TFunc Pure [TBln] (TVar 1)]
-    s = M.fromList [(0, TBln), (1, TInt)]
-
-test07 = unifyTest "07 - typeVar := lambda" c s
-  where
-    c = [ TVar 0 := TFunc Pure [TVar 1] TInt
-        , TVar 0 := TFunc Pure [TBln] (TVar 2) ]
-    s = M.fromList [(0, TFunc Pure [TBln] TInt), (1, TBln), (2, TInt)]
-
-test08 = unifyTest "08 - inc constraints" c s
-  where
-    c = [ TInt := TVar 1
-        , TInt := TInt
-        , TInt := TVar 2
-        , TVar 0 := TFunc Pure [TVar 1] (TVar 2)
-        , TVar 0 := TFunc Pure [TInt] (TVar 3)]
-
-    s = M.fromList
-        [ (0, TFunc Pure [TInt] TInt)
-        , (1, TInt)
-        , (2, TInt)
-        , (3, TInt)
-        ]
