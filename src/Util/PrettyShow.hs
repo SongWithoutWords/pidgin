f7262e49b0c04efa5d60449ab746e6a01b610ffa
ian <ianmccall93@gmail.com>
2017-12-12 13:33:12 -0800

    Improvements to prettyShow

--- src/Util/PrettyShow.hs
+++ src/Util/PrettyShow.hs
@@ -1,15 +1,22 @@
 module Util.PrettyShow(prettyShow) where
 
 prettyShow :: Show a => a -> String
-prettyShow x = prettyShow' (show x) 0
+prettyShow x = prettyShow' (show x) 0 True
   where
 
-prettyShow' :: String -> Int -> String
-prettyShow' [] _ = ""
-prettyShow' (c:cs) n
-  | c == '(' || c == '[' = indentedNewline n ++ c : prettyShow' cs (n+1)
-  | c == ')' || c == ']' = c : prettyShow' cs (n-1)
-  | otherwise = c : prettyShow' cs n
+prettyShow' :: String -> Int -> Bool -> String
+prettyShow' [] _ _ = ""
+prettyShow' (c:cs) n lastCharWasOpener
+
+  | c == '(' || c == '[' =
+    (if lastCharWasOpener then " " else indentedNewline n)
+    ++ c : prettyShow' cs (n+1) True
+
+  | c == ')' || c == ']' = c : prettyShow' cs (n-1) False
+
+  | c == ',' = indentedNewline (n - 1) ++ c : prettyShow' cs n True
+
+  | otherwise = c : prettyShow' cs n False
 
 indentedNewline :: Int -> String
 indentedNewline n = '\n' : replicate (2*n) ' '
