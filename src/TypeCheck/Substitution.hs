a455868593c2206dcdf4a2802fc264f884eb8287
ian <ianmccall93@gmail.com>
2017-12-15 22:46:17 -0800

    Used TVar alias in Substitution

--- src/TypeCheck/Substitution.hs
+++ src/TypeCheck/Substitution.hs
@@ -4,7 +4,7 @@ import qualified Data.Map as M
 
 import Ast.A2Constrained.Type
 
-type Substitutions = M.Map Word Type
+type Substitutions = M.Map TVar Type
 
 subType :: Substitutions -> Type -> Type
 subType s typ = let subType' = subType s in case typ of
