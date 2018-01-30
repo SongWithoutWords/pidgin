11b5bb1596ba24bfed090cae1aad24e0155abfce
ian <ianmccall93@gmail.com>
2018-01-29 18:37:51 -0800

    Began writing PrettyPrinter for A2 and A3

---
+++ src/Ast/A2Constrained/PrettyPrint.hs
@@ -0,0 +1,41 @@
+module Ast.A2Constrained.PrettyPrint where
+
+import Data.List(intercalate)
+
+import Ast.A3Typed
+
+-- printAst :: Ast -> String
+-- printAst = concatMap printUnit
+
+-- printNamedUnit :: Named Unit -> String
+-- printNamedUnit = printUnit' 0
+
+-- printNamedUnit' :: Int -> Named Unit -> String
+-- printNamedUnit' ind unit = case unit of
+--   UNamespace
+
+printExpr :: Expr -> String
+printExpr (Expr _ expr) = case expr of
+  EApp e p args -> (printExpr e) ++ "(" ++ (printPurityAndArgs p args) ++ ")"
+  EName n -> n
+  EIntr i -> nameOfIntrinsic i
+  EIf (Cond a) b c -> printExpr b ++ " if " ++ printExpr a ++ " else " ++ printExpr c
+  EVal v -> printVal v
+
+printPurityAndArgs :: Purity -> [Expr] -> String
+
+printPurityAndArgs p args = intercalate ", " $ (printPurity p) ++ map printExpr args
+
+printPurity :: Purity -> [String]
+printPurity Pure = []
+printPurity PRead = ["@"]
+printPurity PWrite = ["~@"]
+
+printVal :: Value -> String
+printVal v = case v of
+  VBln b -> if b then "true" else "false"
+  VChr c -> "'" ++ show c ++ "'"
+  VFlt f -> show f
+  VInt i -> show i
+  VStr s -> "\"" ++ show s ++ "\""
+
---
+++ src/Ast/A3Typed/PrettyPrint.hs
@@ -0,0 +1,4 @@
+module Ast.A3Typed.PrettyPrint(module X) where
+
+-- re-export A2.PrettyPrint, because A3 is presently an alias for A2
+import Ast.A2Constrained.PrettyPrint as X
